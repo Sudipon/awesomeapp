c4aab4e07c0339d8a755c77815a36a9ac94b5215
Sudipon <60188509+Sudipon@users.noreply.github.com>
2021-10-28 20:23:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import '// import 'bootstrap/dist/css/bootstrap.css';';
+// import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
